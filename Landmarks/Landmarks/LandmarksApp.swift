2bcf543365fb45ceec313c49d54405e59301f9c6
fogbar <kimdongjun6570@gmail.com>
2020-12-16 12:10:15 +0900

    Landmarks Example done

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 김동준 on 2020/12/15.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
